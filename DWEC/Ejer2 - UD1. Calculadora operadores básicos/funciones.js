26c5ad2bbcec7eb19cc4c04801648b78e51e26e3
emilio <emilioblasco@icloud.com>
2022-09-19 14:50:04 +0200

    ejercicio 2

---
+++ DWEC/Ejer2 - UD1. Calculadora operadores básicos/funciones.js
@@ -0,0 +1,9 @@
+const resta = (a,b) => a - b;
+const multiplicacion = (a,b) => a * b;
+const division = (a,b) => a / b;
+
+
+alert(resta(10,3));
+alert(multiplicacion(10,3));
+alert(division(10,3));
+
